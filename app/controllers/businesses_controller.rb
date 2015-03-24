7b2d7cbaee798d38770f4eed92755ffe0d00b94b
Michael Scriven <bigscriv03@gmail.com>
2015-03-24 15:27:32 -0400

    Added business forms and index

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,13 +1,36 @@
 class BusinessesController < ApplicationController
   def index
+    @businesses = Business.all
   end
 
   def new
+    @business = Business.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @business = Business.new(business_params)
+
+    respond_to do |format|
+      if @business.save
+        format.html { redirect_to @business, notice: 'Business was successfully created.' }
+        format.json { render :show, status: :created, location: @business }
+      else
+        format.html { render :new }
+        format.json { render json: @business.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :address, :phone, :website, :image, :category_id)
   end
 end
