f466700bbf42ef9305cb125653441d5d90743edd
Michael Scriven <bigscriv03@gmail.com>
2015-03-26 15:18:57 -0400

    New review form with star rating

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -12,7 +12,12 @@ class BusinessesController < ApplicationController
   end
 
   def show
-    @reviews = Review.where(business_id: @business.id)
+    @reviews = Review.where(business_id: @business.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
 
   def edit
